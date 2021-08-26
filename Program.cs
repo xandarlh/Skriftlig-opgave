26e44ad707515695ab981f2e7e1a360ec168e50f
Alexandar Lackovic Hansen <alex303a@zbc.dk>
2021-08-26 10:12:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skriftlig_opgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
