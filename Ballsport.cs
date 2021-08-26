d5a31f469893880b8ccbfa510d5f39204ee420ea
Alexandar Lackovic <xandarlh@gmail.com>
2021-08-26 16:43:51 +0200

    d

---
+++ Ballsport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skriftlig_opgave
+{
+    public class Ballsport : Sport, IField
+    {
+        public string Playstyle { get; set; }
+        public string BallType { get; set; }
+
+        public Ballsport(string name, string description, string playstyle, string ballType) : base(name, description)
+        {
+            this.Playstyle = playstyle;
+            this.BallType = ballType;
+        }
+
+        public string displayField()
+        {
+            return "Sportfield";
+        }
+    }
+}
